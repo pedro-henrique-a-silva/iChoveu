7e979418f548bd6614c78e2055f875c79c70a109
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-27 16:28:07 -0300

    Requisito 01 concluido, implementando função searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,22 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
+const BASE_URL = 'http://api.weatherapi.com/v1/search.json?lang=pt&';
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const fetchAPI = async (term) => (await fetch(`${BASE_URL}key=${TOKEN}&q=${term}`))
+  .json();
+
+export const searchCities = async (term) => {
+  try {
+    if (!term) throw new Error('Nenhum parametro de pesquisa informado');
+
+    const weatherData = await fetchAPI(term);
+
+    if (weatherData.length === 0) throw new Error('Nenhuma cidade encontrada');
+
+    return weatherData;
+  } catch (error) {
+    alert(error.message);
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
